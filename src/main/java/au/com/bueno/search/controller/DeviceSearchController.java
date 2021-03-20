ec7e68ff40707804da61d887cbe090bb7120c4f0
arun <aruncontacts@gmail.com>
2021-03-20 14:55:22 +1100

    Device by id endpoint integrated

--- src/main/java/au/com/bueno/search/controller/DeviceSearchController.java
+++ src/main/java/au/com/bueno/search/controller/DeviceSearchController.java
@@ -1,12 +1,23 @@
 package au.com.bueno.search.controller;
 
+import au.com.bueno.search.controller.dto.DeviceResponseDto;
+import au.com.bueno.search.service.DeviceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DeviceSearchController {
-  @GetMapping("/")
-  public String index() {
-    return "First req";
+
+  private final DeviceService deviceService;
+
+  public DeviceSearchController(@Autowired DeviceService deviceService) {
+    this.deviceService = deviceService;
+  }
+
+  @GetMapping("/devices/{id}")
+  public DeviceResponseDto index(@PathVariable String id) {
+    return deviceService.findById(id);
   }
 }
---
+++ src/test/java/au/com/bueno/search/controller/DeviceSearchControllerTest.java
@@ -0,0 +1,40 @@
+package au.com.bueno.search.controller;
+
+import au.com.bueno.search.controller.dto.DeviceResponseDto;
+import au.com.bueno.search.service.DeviceService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(DeviceSearchController.class)
+class DeviceSearchControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+  @MockBean
+  private DeviceService service;
+
+  @Test
+  public void greetingShouldReturnMessageFromService() throws Exception {
+    DeviceResponseDto deviceResponseDto = new DeviceResponseDto();
+    deviceResponseDto.setId("abc");
+    deviceResponseDto.setName("name");
+    when(service.findById("abc")).thenReturn(deviceResponseDto);
+    this.mockMvc.perform(get("/devices/abc")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content()
+            .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.name", is("name")))
+        .andExpect(jsonPath("$.id", is("abc")));
+  }
+}
